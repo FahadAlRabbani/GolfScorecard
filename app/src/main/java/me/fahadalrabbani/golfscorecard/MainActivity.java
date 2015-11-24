82a1be687a21c980890f55b80215c65c3dc247f3
Fahad Al Rabbani <alfahad1993@gmail.com>
2015-11-24 12:15:26 +0000

    Initial commit

---
+++ app/src/main/java/me/fahadalrabbani/golfscorecard/MainActivity.java
@@ -0,0 +1,69 @@
+package me.fahadalrabbani.golfscorecard;
+
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends ListActivity {
+    private static final String PREFS_FILE = "me.fahadalrabbani.golfscorecard.preferences";
+    private static final String KEY_STROKECOUNT = "KEY_STROKECOUNT";
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+    private Hole[] mHoles = new Hole[18];
+    private ListAdapter mListAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mSharedPreferences = getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE);
+        mEditor = mSharedPreferences.edit();
+        int strokes = 0;
+
+        for (int i = 0; i < mHoles.length; i++) {
+            strokes = mSharedPreferences.getInt(KEY_STROKECOUNT + i, 0);
+            mHoles[i] = new Hole(String.format("Hole %d : ", i + 1), strokes);
+        }
+
+        mListAdapter = new ListAdapter(getApplicationContext(),mHoles);
+        setListAdapter(mListAdapter);
+        
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        for (int i = 0; i < mHoles.length; i++){
+            mEditor.putInt(KEY_STROKECOUNT + i,mHoles[i].getStrokeCount());
+        }
+
+        mEditor.apply();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id  = item.getItemId();
+
+        if (id == R.id.action_clearstroke){
+            mEditor.clear();
+            mEditor.apply();
+            for (Hole hole:mHoles) {
+                hole.setStrokeCount(0);
+            }
+            mListAdapter.notifyDataSetChanged();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
